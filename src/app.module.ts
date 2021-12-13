573807ce70b37da1d879b43923d6905cc754b7b1
Ken Ovienadu <kenovienadu@gmail.com>
2021-12-13 13:07:13 +0100

    feat: setup sequelize orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadModels: true
+      }),
+      inject: [ConfigService],
+    })
+  ],
   controllers: [],
   providers: [],
 })
